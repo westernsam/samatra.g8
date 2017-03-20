8205011605b3adc6377a6f942c54b74391a47255
sam <sam.owen.consultant@springer.com>
2017-03-20 13:46:13 +0000

    Add template readme

---
+++ src/main/g8/README.md
@@ -0,0 +1,7 @@
+## $name
+
+
+## To run locally add the following to the VM options of Main:
+```
+-javaagent:tools/property-loading/loadenv.jar=.runenv
+```
---
+++ src/main/g8/src/main/scala/$package$/LogErrors.scala
@@ -0,0 +1,22 @@
+package $package$
+
+
+import java.io.Writer
+import javax.servlet.http.HttpServletRequest
+
+import com.springer.samatra.routing.Request
+import com.springer.samatra.extras.Logger
+import org.eclipse.jetty.server.handler.ErrorHandler
+
+class LogErrors extends ErrorHandler with Logger {
+  override def writeErrorPage(request: HttpServletRequest, writer: Writer, code: Int, message: String, showStacks: Boolean): Unit = {
+    val uri = Request(request).toUri
+
+    if (code >= 500)
+      log.error(s"Application error: version uri [\$uri]", Option(request.getAttribute("javax.servlet.error.exception")).map(_.asInstanceOf[Exception]))
+
+    //write custom error page to writer if required
+    super.writeErrorPage(request, writer, code, message, showStacks)
+  }
+}
+
