fd5e7dce8e8cc8e4d48efe8a3d1d52537417d46b
sam <sam.owen.consultant@springer.com>
2017-10-27 17:06:17 +0100

    Sam: upgrade to samatra-extras 1.7.1 with added metrics

--- src/main/g8/src/main/scala/$package$/WebApp.scala
+++ src/main/g8/src/main/scala/$package$/WebApp.scala
@@ -1,15 +1,15 @@
 package $package$
 
+import com.springer.samatra.extras._
+import com.springer.samatra.extras.asynchttp.MetricsCollectingAsyncHttp
 import com.springer.samatra.extras.logging.NoOpRequestLog
-import com.springer.samatra.extras.metrics.{GzippedMetricsEnabledHandler, JvmMetricsCollector, JvmMetricsCollectorStatsDService, MetricsStatsdClient}
+import com.springer.samatra.extras.metrics.MetricsHandlers.gzipWebAndRouteMetrics
+import com.springer.samatra.extras.metrics._
 import com.springer.samatra.extras.responses.{MustacheRenderer, TemplateRenderer}
-import com.springer.samatra.extras.{Logger, RoutePrinting, WebServer, WebappContextHandler}
 import com.timgroup.statsd.{NonBlockingStatsDClient, StatsDClient}
-import org.asynchttpclient.{AsyncHttpClient, DefaultAsyncHttpClient, DefaultAsyncHttpClientConfig}
+import org.asynchttpclient.{AsyncHttpClient, DefaultAsyncHttpClientConfig}
 import org.eclipse.jetty.server.Slf4jRequestLog
 import org.eclipse.jetty.server.handler.RequestLogHandler
-
-import scala.concurrent.ExecutionContext
 
 object WebApp extends Logger {
 
@@ -28,17 +28,19 @@ object WebApp extends Logger {
   val jvmstats = new JvmMetricsCollectorStatsDService(new JvmMetricsCollector, statsd, sys.env.getOrElse("CF_INSTANCE_INDEX", "LOCAL"))
   val statsdClient = MetricsStatsdClient.newStatsDClient(statsd, sys.env("ENV"))
 
-  val http: AsyncHttpClient = new DefaultAsyncHttpClient(new DefaultAsyncHttpClientConfig.Builder()
+  val http: AsyncHttpClient = MetricsCollectingAsyncHttp(new DefaultAsyncHttpClientConfig.Builder()
     .setIoThreadsCount(1)
     .setKeepAlive(true)
     .setConnectTimeout(1000)
     .setRequestTimeout(2000)
     .setMaxConnectionsPerHost(20)
-    .setFollowRedirect(true).build()
-  )
+    .setFollowRedirect(true),
+    statsdClient,
+    req => req.getUri.getHost.replaceAll("\\\\.", "-")
+    )
 
-  def apply(port: Int = 0) = new WebServer(port)
-    .addHandler(new GzippedMetricsEnabledHandler(new WebappContextHandler with RoutePrinting {}
+  def apply(port: Int = 0) : WebServer = new WebServer(port)
+    .addHandler(gzipWebAndRouteMetrics(new WebappContextHandler with RoutePrinting {}
       .addRoutes("/example/*",
         new TemplateExampleController(),
         new ProxyExampleController(http)
